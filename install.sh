16685cd560f90f9f955b219c3ca8c2c4d0946ee0
Tony Destro <tony.destro@gmail.com>
2018-09-08 14:55:56 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@ echo Tony\'s Arch Linux Installer Script.
 timedatectl set-ntp true
 pacman --noconfirm --sync --refresh terminus-font f2fs-tools
 setfont ter-132n
-mkfs.ext4 /dev/nvme0n1p4
+mkfs.ext4 -f /dev/nvme0n1p4
 mkswap /dev/nvme0n1p5
 swapon /dev/nvme0n1p5
-mkfs.f2fs /dev/nvme0n1p6
+mkfs.f2fs -f /dev/nvme0n1p6
 mount /dev/nvme0n1p6 /mnt
 mkdir /mnt/boot
 mount /dev/nvme0n1p4 /mnt/boot
