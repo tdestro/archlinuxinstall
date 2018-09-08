f2cac5bfe928f6ac24f4caa99ea2e5f6cff8f416
Tony Destro <tony.destro@gmail.com>
2018-09-08 13:13:25 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 echo "Tony's Arch Linux Installer Script"
+# Fix the unreadable screen.
+pacman --sync --refresh terminus-font --noconfirm
+setfont /usr/share/consolefonts/ter-132n.psf.gz
