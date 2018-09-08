ef13ecb0302184861c589c14ac84b7cd0f371dee
Tony Destro <tony.destro@gmail.com>
2018-09-08 13:56:33 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-echo Tony's Arch Linux Installer Script.
+echo Tony\'s Arch Linux Installer Script.
 pacman --sync --refresh terminus-font --noconfirm
 setfont /usr/share/consolefonts/ter-132n.psf.gz
