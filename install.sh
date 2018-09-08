880d931b6a0b3d74396bd678439166c68bbeb367
Tony Destro <tony.destro@gmail.com>
2018-09-08 17:25:40 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,15 @@ mkfs.f2fs -f /dev/nvme0n1p6
 mount /dev/nvme0n1p6 /mnt
 mkdir /mnt/boot
 mount /dev/nvme0n1p4 /mnt/boot
-pacstrap /mnt base base-devel grub efibootmgr dosfstools os-prober mtools f2fs-tools 
+pacstrap /mnt base base-devel grub efibootmgr dosfstools os-prober mtools f2fs-tools bash-completion \
+xorg-server xorg-xinit xorg-utils xorg-server-utils mesa xorg-twm xterm xorg-xclock xf86-input-synaptics cinnamon nemo-fileroller gdm \
+pulseaudio pulseaudio-alsa pavucontrol gnome-terminal firefox flashplugin vlc chromium unzip unrar p7zip pidgin skype deluge smplayer audacious qmmp gimp xfburn thunderbird gedit gnome-system-monitor \
+a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore gstreamer0.10-plugins \
+yaourt \
+Faenza-icon-theme numix-themes \
+libgtop networkmanager \
+git go \
+nvidia
 genfstab -U /mnt > /mnt/etc/fstab
 
 ###############################
@@ -38,4 +46,7 @@ mkdir /boot/EFI
 mount /dev/nvme0n1p1 /boot/EFI  #Mount FAT32 EFI partition 
 grub-install --target=x86_64-efi  --bootloader-id=grub_uefi --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
+grub-mkfont --output=/boot/grub/fonts/DejaVuSansMono20.pf2 \ --size=20 /usr/share/fonts/TTF/dejavu/DejaVuSansMono.ttf
+echo "GRUB_FONT=/boot/grub/fonts/DejaVuSansMono18.pf2" >> /etc/default/grub 
+update-grub
 EOF
