3197186e452d3d196183286abb1c0006bf52b44d
Tony Destro <tony.destro@gmail.com>
2018-09-08 14:38:35 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 echo Tony\'s Arch Linux Installer Script.
+timedatectl set-ntp true
 pacman --noconfirm --sync --refresh terminus-font 
 setfont ter-132n
+mkswap /dev/nvme0n1p5
+swapon /dev/nvme0n1p5
