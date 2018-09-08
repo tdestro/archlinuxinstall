788a2ccb2792d42dd56be01b79dc893fa7b0d25e
Tony Destro <tony.destro@gmail.com>
2018-09-08 15:16:50 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,3 +11,27 @@ mount /dev/nvme0n1p6 /mnt
 mkdir /mnt/boot
 mount /dev/nvme0n1p4 /mnt/boot
 pacstrap /mnt base base-devel
+genfstab -U /mnt >> /mnt/etc/fstab
+
+###############################
+#### Configure base system ####
+###############################
+arch-chroot /mnt /bin/bash <<EOF
+echo "Setting and generating locale"
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+locale-gen
+export LANG=en_US.UTF-8
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "Setting time zone"
+ln -s /usr/share/zoneinfo/EST /etc/localtime
+echo "Setting hostname"
+echo "precision5530" > /etc/hostname
+sed -i "/localhost/s/$/ precision5530/" /etc/hosts
+echo "Installing wifi packages"
+pacman --noconfirm -S iw wpa_supplicant dialog wpa_actiond
+echo "Generating initramfs"
+sed -i 's/^HOOKS.*/HOOKS="base udev autodetect modconf block encrypt lvm2 filesystems keyboard fsck"/' /etc/mkinitcpio.conf
+mkinitcpio -p linux
+echo "Setting root password"
+echo "root:baloney1" | chpasswd
+EOF
