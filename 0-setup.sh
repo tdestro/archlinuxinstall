e734873cce75cba87d30189c00315296e47d1c73
Tony Destro <tony.destro@gmail.com>
2018-09-08 21:21:06 -0400

    Update 0-setup.sh

--- 0-setup.sh
+++ 0-setup.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+timedatectl set-ntp true
+pacman --noconfirm --sync --refresh terminus-font f2fs-tools
+setfont ter-132n
 git config --global user.email "tony.destro@gmail.com"
 git config --global user.name "Tony Destro"
