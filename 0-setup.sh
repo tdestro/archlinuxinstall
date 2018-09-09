2885065ef49101649dcb6ad5a9c3563d7c7f2682
Tony Destro <tony.destro@gmail.com>
2018-09-09 11:24:18 -0400

    Update 0-setup.sh

--- 0-setup.sh
+++ 0-setup.sh
@@ -5,5 +5,6 @@ git config --global user.name "Tony Destro"
 sed -i '/^#\[multilib\]/s/^#//' /etc/pacman.conf
 sed -i "$(( `grep -n "^\[multilib\]" /etc/pacman.conf | cut -f1 -d:` + 1 ))s/^#//" /etc/pacman.conf
 echo "Server = http://mirror.cs.pitt.edu/archlinux/$repo/os/$arch" >> /etc/pacman.d/mirrorlist
-pacman --noconfirm -Syyu --refresh terminus-font f2fs-tools
+pacman --noconfirm -Syyuu
+pacman --noconfirm -S --refresh terminus-font f2fs-tools
 setfont ter-132n
