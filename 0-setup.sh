9228f4d2f44ca3482f14aeb1f4278d6afb70f7aa
Tony Destro <tony.destro@gmail.com>
2018-09-09 10:54:44 -0400

    Update 0-setup.sh

--- 0-setup.sh
+++ 0-setup.sh
@@ -4,4 +4,5 @@ pacman --noconfirm --sync --refresh terminus-font f2fs-tools
 setfont ter-132n
 git config --global user.email "tony.destro@gmail.com"
 git config --global user.name "Tony Destro"
+sed -i 's/#[multilib]/[multilib]/' /etc/pacman.conf
 echo "Server = http://mirror.cs.pitt.edu/archlinux/$repo/os/$arch" >> /etc/pacman.d/mirrorlist
