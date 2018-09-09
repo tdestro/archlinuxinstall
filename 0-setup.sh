af9367fc2e58af431053f48323395c467f99c94b
Tony Destro <tony.destro@gmail.com>
2018-09-09 11:03:33 -0400

    Update 0-setup.sh

--- 0-setup.sh
+++ 0-setup.sh
@@ -4,6 +4,7 @@ pacman --noconfirm --sync --refresh terminus-font f2fs-tools
 setfont ter-132n
 git config --global user.email "tony.destro@gmail.com"
 git config --global user.name "Tony Destro"
-sed -i 's/#[multilib]/[multilib]/' /etc/pacman.conf
+sed -i '/^#\[multilib\]/s/^#//' /etc/pacman.conf
+sed -i "$(( `grep -n "^\[multilib\]" /etc/pacman.conf | cut -f1 -d:` + 1 ))s/^#//" /etc/pacman.conf
 echo "Server = http://mirror.cs.pitt.edu/archlinux/$repo/os/$arch" >> /etc/pacman.d/mirrorlist
-pacman -Sy
+pacman -Syyu
