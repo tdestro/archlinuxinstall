0d7b757f6d8648b77a9a4e2d770dd6192ea4039f
Tony Destro <tony.destro@gmail.com>
2018-09-11 08:32:16 -0400

    Update 0-setup.sh

--- 0-setup.sh
+++ 0-setup.sh
@@ -4,11 +4,7 @@ git config --global user.email "tony.destro@gmail.com"
 git config --global user.name "Tony Destro"
 sed -i '/^#\[multilib\]/s/^#//' /etc/pacman.conf
 sed -i "$(( `grep -n "^\[multilib\]" /etc/pacman.conf | cut -f1 -d:` + 1 ))s/^#//" /etc/pacman.conf
-echo "Server = http://mirrors.advancedhosters.com/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
-echo "Server = http://mirror.wdc1.us.leaseweb.net/archlinux/\$repo/os/\$arch" >> /etc/pacman.d/mirrorlist
-echo "Server = http://mirror.cs.pitt.edu/archlinux/\$repo/os/\$arch" >> /etc/pacman.d/mirrorlist
-echo "Server = http://arch.mirrors.pair.com/\$repo/os/\$arch" >> /etc/pacman.d/mirrorlist
-echo "Server = http://mirror.vtti.vt.edu/archlinux/\$repo/os/\$arch" >> /etc/pacman.d/mirrorlist
-echo "Server = http://mirror.csclub.uwaterloo.ca/archlinux/\$repo/os/\$arch" >> /etc/pacman.d/mirrorlist
+rm /etc/pacman.d/mirrorlist
+cp ./mirrorlist /etc/pacman.d/mirrorlist
 pacman --noconfirm -Sy --refresh terminus-font f2fs-tools
 setfont ter-132n
