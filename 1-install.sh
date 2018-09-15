f0d6bf37dd97866f5fd0038f2fa4b1a1db353e9d
Tony Destro <tony.destro@gmail.com>
2018-09-15 14:18:00 -0400

    Update 1-install.sh

--- 1-install.sh
+++ 1-install.sh
@@ -196,7 +196,7 @@ rm -rf /home/tdestro/yay
 su tdestro -c 'echo "baloney1" | yay -S --noconfirm --noprovides jetbrains-toolbox debtap virtualbox-ext-oracle xarchiver-gtk2 lightdm-webkit2-greeter lightdm-webkit-theme-aether acroread nemo-compare kalu vertex-themes-git cinnamon-applet-cpu-temperatur-git cinnamon-applet-hardware-monitor cinnamon-sound-effects jdownloader2 visual-studio-code-bin gitkraken postman-bin simplenote-electron-bin' 
 
 # avatar from github for light dm
-su tdestro -c 'curl –sS –output /home/tdestro/.face https://avatars0.githubusercontent.com/u/10113013'
+su tdestro -c 'curl https://avatars0.githubusercontent.com/u/10113013 -o /home/tdestro/.face'
 
 # gcloud 
 curl https://sdk.cloud.google.com -o ./install-gcloud.sh && \
