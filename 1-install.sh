583cc49be329b9191ca3b95891df6dd285aef1b4
Tony Destro <tony.destro@gmail.com>
2018-09-16 15:16:57 -0400

    Update 1-install.sh

--- 1-install.sh
+++ 1-install.sh
@@ -15,8 +15,10 @@ set -o history -o histexpand
 
 # ensure drives are unmounted, even if it's stupid.
 umount -l /dev/nvme0n1p4 || [ $? -eq 1 ]
+swapoff /dev/nvme0n1p5 || [ $? -eq 1 ]
 umount -l /dev/nvme0n1p5 || [ $? -eq 1 ]
 umount -l /dev/nvme0n1p6 || [ $? -eq 1 ]
+
 
 mkfs.ext4 -F /dev/nvme0n1p4
 exit_on_error $? !!
