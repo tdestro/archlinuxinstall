e03ff94872858117a731bd10fa551495afdde54e
Tony Destro <tony.destro@gmail.com>
2019-08-03 18:19:44 -0400

    Update 1-repair.sh

--- 1-repair.sh
+++ 1-repair.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 mount /dev/nvme0n1p6 /mnt
 mount /dev/nvme0n1p4 /mnt/boot
-arch-chroot /mnt /bin/bash
+arch-chroot /mnt /bin/bash'
+mount /dev/nvme0n1p1 /boot/EFI
+grub-install --target=x86_64-efi  --bootloader-id=grub_uefi --recheck
