73109a81be4ca31546feec3ea3b5df9b1ff18c74
Tony Destro <tony.destro@gmail.com>
2019-10-21 21:17:50 +0000

    fix repair

--- 1-repair.sh
+++ 1-repair.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-mount /dev/nvme0n1p6 /mnt
-mount /dev/nvme0n1p4 /mnt/boot
-arch-chroot /mnt /bin/bash'
-mount /dev/nvme0n1p1 /boot/EFI
+mount /dev/nvme0n1p7 /mnt
+mount /dev/nvme0n1p5 /mnt/boot
+arch-chroot /mnt /bin/bash <<EOF
+mount /dev/nvme0n1p2 /boot/EFI
 grub-install --target=x86_64-efi  --bootloader-id=grub_uefi --recheck
+EOF
